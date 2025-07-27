185ef41e95befce26413c6dbfe8c1a3bc67d0e4b
Rama <ramabairagoni22@gmail.com>
2025-07-27 18:13:40 +0530

    selenium

---
+++ SIBIIB.java
@@ -0,0 +1,28 @@
+package javaprograms;
+
+public class SIBIIB {
+
+	
+		static
+		{
+			System.out.println("SIB");
+			
+		}
+		{
+			System.out.println("IIB");
+		}
+		
+		SIBIIB()
+		{
+			System.out.println("constructor");
+		}
+		
+		public static void main(String[] args) 
+		{
+
+			System.out.println("main method");
+			new SIBIIB();
+	
+	}
+
+}
